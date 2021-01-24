46c8a38e154da67718bb03b0a851c7896354bcdc
melda_celik <melda_celik@hotmail.com>
2021-01-24 18:52:11 +0300

    Dictionary eklendi

---
+++ Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> ornekSozluk = new Dictionary<string, string>();
+            ornekSozluk.Add("pzt", "Pazartesi");
+            ornekSozluk.Add("sal", "Salı");
+            ornekSozluk.Add("çar", "Çarşamba");
+            ornekSozluk.Add("per", "Perşembe");
+            ornekSozluk.Add("cu", "Cuma");
+            ornekSozluk.Add("cmt", "Cumartesi");
+            ornekSozluk.Add("paz", "Pazar");
+
+            Console.WriteLine("Örnek sözlük: " + ornekSozluk["pzt"]);
+
+            Console.WriteLine("Örnek anahtar kontrol: " + ornekSozluk.ContainsKey("sal"));
+            Console.WriteLine("Örnek anahtar kontrol: " + ornekSozluk.ContainsKey("çrş"));
+        }
+    }
+}
